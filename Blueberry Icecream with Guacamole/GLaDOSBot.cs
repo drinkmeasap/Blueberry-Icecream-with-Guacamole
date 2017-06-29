689b3c9739ddb22a6f28a972758a33557dfd2ba0
Wilswe <william.sandbrink@gmail.com>
2017-06-29 13:43:54 +0200

    All the stuff

---
+++ Blueberry Icecream with Guacamole/GLaDOSBot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blueberry_Icecream_with_Guacamole
+{
+    class GLaDOSBot
+    {
+    }
+}
---
+++ Blueberry Icecream with Guacamole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blueberry_Icecream_with_Guacamole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GLaDOSBot bot = new GLaDOSBot();
+        }
+    }
+}
